8a4dff9d743cdb90f3c3eeb5e8541459befff741
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-05-24 22:26:18 +0100

    update

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,6 +1,37 @@
 // TODO: Write your class in this file
+class SoftPLay {
+  constructor() {
+    this.adults = 0
+    this.children = 0
+  }
+  occupancy() {
+    return {'adults': this.adults, 'children': this.children}
+  }
+  enter(a,c) {
+    if (c <= a) {
+      this.adults += a
+      this.children += c
+      return true
+    }
+    else {
+      return false
+    }
+  }
+  leave(a,c){
+    if (a<=this.adults && c<=this.children && c <= a && this.adults-a >= this.children-c) {
+      this.adults -= a
+      this.children -= c
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+}
 
 // TODO: Change undefined to the name of your class
 module.exports = {
-  SoftPlay: undefined
+  SoftPlay: SoftPLay
 }
+
